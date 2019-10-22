2c8eb06849bf641a8d1fc35de452a49ec9807b15
Ola Flesche <olhellen@nmbu.no>
2019-10-22 14:17:53 +0200

    Starting ex04

---
+++ src/ola_flesche_ex/ex04/myrand.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ola Flesche Hellenes'
+__email__ = 'olhellen@nmbu.no'
+
+import random as rd
+
+class LCGRand:
+    def __init__(self, seed()):
+        a = 7**5
+        m = 2**31-1
+        while True:
+            if number
+        self.rand[n+1] = a*r[n] mod m
+        retuns self.rand
+
+
+class ListRand:
+    def __init__(self, rand()):
+        self.ListRand
+        if
