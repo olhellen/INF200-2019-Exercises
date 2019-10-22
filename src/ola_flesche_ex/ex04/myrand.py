2a8144e41e5187d225829c68e7631c6436698bae
Ola Flesche <olhellen@nmbu.no>
2019-10-22 16:59:26 +0200

    LCGRand done, start to ListRand

--- src/ola_flesche_ex/ex04/myrand.py
+++ src/ola_flesche_ex/ex04/myrand.py
@@ -3,19 +3,41 @@
 __author__ = 'Ola Flesche Hellenes'
 __email__ = 'olhellen@nmbu.no'
 
-import random as rd
 
 class LCGRand:
-    def __init__(self, seed()):
-        a = 7**5
-        m = 2**31-1
+    def __init__(self, seed):
+        self.seed = seed
+        self.a = 7**5
+        self.m = 2**(31)-1
+
+
+    def rand(self):
         while True:
-            if number
-        self.rand[n+1] = a*r[n] mod m
-        retuns self.rand
+            self.seed = (self.a * self.seed) % self.m
+            return self.seed
 
 
 class ListRand:
-    def __init__(self, rand()):
-        self.ListRand
-        if
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+
+    def rand(self):
+        for number in range(self.numbers):
+            if number <= len(self.numbers):
+                print(number)
+            else:
+                raise RuntimeError
+
+
+def test_list_rng():
+    """Test that ListRNG generator works."""
+    numbers = [4, 5, 29, 11]
+    lr = ListRand(numbers)
+    assert [lr.rand() for _ in range(len(numbers))] == numbers
+    with pytest.raises(RuntimeError):
+        lr.rand()
+
+
+
+
---
+++ src/ola_flesche_ex/ex04/walker.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ola Flesche Hellenes'
+__email__ = 'olhellen@nmbu.no'
+
+class walker:
+    def __init__
