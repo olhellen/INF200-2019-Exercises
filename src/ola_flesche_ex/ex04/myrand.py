27350fd3cce5a8d4b11ba011b298f9fd43f3edf8
Ola Flesche <olhellen@nmbu.no>
2019-10-24 17:48:43 +0200

    Finished the walker class, struggling with list_rng

--- src/ola_flesche_ex/ex04/myrand.py
+++ src/ola_flesche_ex/ex04/myrand.py
@@ -8,8 +8,7 @@ class LCGRand:
     def __init__(self, seed):
         self.seed = seed
         self.a = 7**5
-        self.m = 2**(31)-1
-
+        self.m = 2**31-1
 
     def rand(self):
         while True:
@@ -21,23 +20,9 @@ class ListRand:
     def __init__(self, numbers):
         self.numbers = numbers
 
-
     def rand(self):
-        for number in range(self.numbers):
+        for number in enumerate(self.numbers):
             if number <= len(self.numbers):
-                print(number)
+                return
             else:
                 raise RuntimeError
-
-
-def test_list_rng():
-    """Test that ListRNG generator works."""
-    numbers = [4, 5, 29, 11]
-    lr = ListRand(numbers)
-    assert [lr.rand() for _ in range(len(numbers))] == numbers
-    with pytest.raises(RuntimeError):
-        lr.rand()
-
-
-
-
--- src/ola_flesche_ex/ex04/walker.py
+++ src/ola_flesche_ex/ex04/walker.py
@@ -2,6 +2,55 @@
 
 __author__ = 'Ola Flesche Hellenes'
 __email__ = 'olhellen@nmbu.no'
+import random
 
-class walker:
-    def __init__
+
+class Walker:
+    def __init__(self, x0, h):
+        self.x0 = x0
+        self.h = h
+        self.steps = 0
+
+    def move(self):
+        self.steps += 1
+        step = 2*random.randint(0, 1)-1
+        self.x0 = self.x0 + step
+
+    def is_at_home(self):
+        if self.x0 == self.h:
+            return True
+
+    def get_position(self):
+        return self.x0
+
+    def get_steps(self):
+        return self.steps
+
+    def walking(self):
+        while not self.is_at_home():
+            self.move()
+        return self.steps
+
+
+if __name__ == "__main__":
+    print(
+        f'Distance:   1 -> Path lengths:' 
+        f'{sorted([Walker(0,1).walking() for step in range(5)])}')
+    print(
+        f'Distance:   2 -> Path lengths:' 
+        f'{sorted([Walker(0, 2).walking() for step in range(5)])}')
+    print(
+        f'Distance:   5 -> Path lengths:' 
+        f'{sorted([Walker(0, 5).walking() for step in range(5)])}')
+    print(
+        f'Distance:   10 -> Path lengths:' 
+        f'{sorted([Walker(0, 10).walking() for step in range(5)])}')
+    print(
+        f'Distance:   20 -> Path lengths:' 
+        f'{sorted([Walker(0, 20).walking() for step in range(5)])}')
+    print(
+        f'Distance:   50 -> Path lengths:' 
+        f'{sorted([Walker(0, 50).walking() for step in range(5)])}')
+    print(
+        f'Distance:   100 -> Path lengths:' 
+        f'{sorted([Walker(0, 100).walking() for step in range(5)])}')
