00422643326952b10fd17edf89ab192b5a6e24ac
Ola Flesche <olhellen@nmbu.no>
2019-11-01 17:56:18 +0100

    changed to enumerate in my rand main test

--- src/ola_flesche_ex/ex05/myrand.py
+++ src/ola_flesche_ex/ex05/myrand.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Ola Flesche Hellenes'
 __email__ = 'olhellen@nmbu.no'
-import random
+
 
 class LCGRand:
     def __init__(self, seed):
@@ -10,6 +10,7 @@ class LCGRand:
         self.a = 7 ** 5
         self.m = 2 ** 31 - 1
         self.i = 0
+
     def rand(self):
         self.seed = (self.a * self.seed) % self.m
         return self.seed
@@ -18,8 +19,7 @@ class LCGRand:
         return RandIter(self, length)
 
     def infinite_random_sequence(self):
-        while False:
-            yield RandIter(self, i)
+        return RandIter(self, -1)
 
 
 class RandIter:
@@ -48,7 +48,8 @@ if __name__ == "__main__":
     for rand in random_number_generator.random_sequence(10):
         print(rand)
 
-    for i, rand in random_number_generator.infinite_random_sequence():
+    for (i, rand) in \
+            enumerate(random_number_generator.infinite_random_sequence()):
         print(f'The {i}-th random number is {rand}')
         if i > 100:
             break
